5f770270886f8b7f8727413c8c166ada344f41f2
Dave86dev <davidochando1986@gmail.com>
2024-06-10 12:33:36 +0200

    feat: kick off project node first steps

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// let {Sumar, Restar} = require("./friend");
+
+// let resultado = Sumar(1,4)
+
+// console.log(resultado)
+
+let {Sumar, Restar, Multiplicar, Dividir} = require("./friend");
+let resultado = Multiplicar(1,4)
+// let resultado2 = Dividir 
+console.log(resultado)
